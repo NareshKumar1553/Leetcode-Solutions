1db91dd47053a88d49b63c6f6121cc74c53b16b4
NareshKumar1553 <nareshkumar.s1553@gmail.com>
2024-06-01 12:26:40 +0530

    commit

---
+++ Easy/3110_Score of a String.java
@@ -0,0 +1,20 @@
+/*You are given a string s. 
+The score of a string is defined as the sum of the 
+absolute difference between the ASCII values of adjacent characters.
+Return the score of s.*/
+
+
+class Solution {
+    public static int scoreOfString(String s) {
+        int res = 0;
+        for(int i=0;i<s.length()-1;i++){
+            res += Math.abs((int)s.charAt(i) - (int)s.charAt(i+1));
+        }
+        return res;
+    }
+
+    public static int main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.scoreOfString("abc"));
+    }
+}
