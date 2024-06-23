136eb2166a5215be8f83fc9eeb5b39b5e5a57ea4
NareshKumar1553 <nareshkumar.s1553@gmail.com>
2024-06-23 12:40:55 +0530

    Daily Challenge Completed

---
+++ Medium/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @param {number} limit
+ * @return {number}
+ */
+var longestSubarray = function (nums, limit) {
+  let max = [],
+    min = [],
+    start = 0,
+    end = 0,
+    ans = 0;
+  //iterate till end
+  while (end < nums.length) {
+    //update the max and the min queues
+    while (max.length > 0 && max[max.length - 1] < nums[end]) {
+      max.pop();
+    }
+    while (min.length > 0 && min[min.length - 1] > nums[end]) {
+      min.pop();
+    }
+    max.push(nums[end]);
+    min.push(nums[end]);
+    //shrink the window if the max and min are out of bounds
+    while (max[0] - min[0] > limit) {
+      if (max[0] == nums[start]) {
+        max.shift();
+      }
+      if (min[0] == nums[start]) {
+        min.shift();
+      }
+      start++;
+    }
+    ans = Math.max(ans, end - start + 1);
+    end++;
+  }
+  return ans;
+};
